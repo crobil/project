1e33b3dc04c04a9fd7fa64f0cecce5c548d83682
root <root@localhost.localdomain>
2018-04-03 17:51:40 +0900

    commit

---
+++ sample2.py
@@ -0,0 +1,17 @@
+import sys
+
+"""
+instruction
+
+add(1)(2)(3); // 6
+add(1)(2)(3)(4); // 10
+add(1)(2)(3)(4)(5); // 15
+"""
+
+class add(int):    # could also subclass float
+    def __call__(self, value):
+		return add(self + value)
+
+print add(1)
+print add(1)(2)
+print add(1)(2)(3)

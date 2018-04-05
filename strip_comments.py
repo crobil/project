62bcd24841917e275317803b4608812bd22745b8
root <root@localhost.localdomain>
2018-04-05 11:25:19 +0900

    add algorithm

--- strip_comments.py
+++ strip_comments.py
@@ -21,22 +21,32 @@ The code would be called like so:
 result = solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"])
 # result should == "apples, pears\ngrapes\nbananas"
 """
-def solution(string,markers):
-	out = ''
-	flag = True
-	for x in string:
-		if x in markers:
-			flag = False
-		elif x in '\n':
-			flag = True
-		if flag:
-			out += x
-			
-	out = out.replace(' \n','\n')
-	if out[-1] == ' ':
-		out = out[:-1]
-	return out
 
+def solution(string,markers):
+	#your code here
+	for marker in markers:
+		string = string.split("\n")
+		sArr = []
+		for s in string:
+			str1 = s.split(marker)
+			sArr.append(str1[0].strip())
+		strP = ""
+		n = 0
+		for p in sArr:
+			if n==0:
+				strP = strP + p
+			else:
+				strP = strP + "\n" + p
+			n+=1
+		string = strP
+	return string
+"""
+def solution(string,markers):
+	parts = string.split('\n')
+	for s in markers:
+		parts = [v.split(s)[0].rstrip() for v in parts]
+	return '\n'.join(parts)
+"""
 print solution('a #b\nc\nd $e f g', ['#', '$'])
 print solution('apples, pears # and bananas\ngrapes\nbananas !apples', ['#', '!'])
 print solution('apples, pears \\xc2\\xa7 and bananas\ngrapes\navocado', ['\\x'])
